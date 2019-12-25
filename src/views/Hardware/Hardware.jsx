6fdb2f16af012e44d6beba8fb8285dffe7061b93
Haji Uzdenov <h.uzdenov@azintex.com>
2019-12-25 18:44:10 +0400

    minor changes

--- src/views/Hardware/Hardware.jsx
+++ src/views/Hardware/Hardware.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from 'react';
-//import useFetch from './FetchHook';
-
+import { func } from 'prop-types';
 
 const Hardware = () => {
-
-    const [hardwares, getHardwares] = useState([])
+    const [name, setName] = useState('')
+    const [isAdded, setAdd] = useState(false)
+    const [names, ] = useState([])
 
     useEffect(() => {
-        fetch('http://api.azintex.com/hardware').then(res => res.json())
-    }, []);
+        console.log(name);
+    },[name])
 
     return(
         <div>
-            <button onClick={() => getHardwares()}>Click</button>
+            <ul>
+                {names.map(_name => <li key={Math.random(1500)}>{_name}</li>)}
+            </ul>
+            <input type='text' name='name' placeholder='Type name here' value={name} onChange={(e) => setName(e.target.value)} ></input>
+            <button onClick={() => setAdd(!isAdded)}>Add name</button>
         </div>
     )
 }
 
-export default Hardware
+export default Hardware;
